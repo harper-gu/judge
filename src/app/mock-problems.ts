559c1f7a2317961e52a6d5ab8065fbb2197572cb
Yejun Gu <yjgu@berkeley.edu>
2017-05-10 17:28:12 -0700

    problem-list component fixed

--- src/app/mock-problems.ts
+++ src/app/mock-problems.ts
@@ -3,8 +3,14 @@ import { Problem } from './models/problem.model';
 export const PROBLEMS: Problem[] = [
   {
     id:0,
-    name:"test",
+    name:"test0",
     desc:"test content",
     difficulty: "easy"
+  },
+  {
+    id:1,
+    name:"test1",
+    desc:"test content 1",
+    difficulty: "mediun"
   }
 ];
